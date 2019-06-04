6c4f364e7cdfe375eb4ece15cc0d4af912cbb236
和也山本 <kazuya2137@gmail.com>
2019-06-04 15:41:57 +0800

    問１
    作業途中

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php 
+
+//  次の「Kidクラス」を作ってください。
+
+// プロパティ：名前($name)、性別($sex)
+// メソッド：showName()
+// 男なら「○○くん」、女なら「○○ちゃん」を返す処理を行う
+// 名前プロパティの初期値は「Seed」、性別プロパティの初期値は「boy」にすること
+// プロパティのゲッター・セッターメソッドを用意し、メソッド経由でプロパティにアクセスできるようにすること
+// オブジェクトからプロパティ値を設定し、showNameメソッドを呼び出して出力すること
+$name= new Name();
+$sex= new Sex();
+class Name{
+
+  $name;
+
+  function __construct(){
+    $this->name = 'seed';
+    $this->sex = 'boy';
+  }
+  public function showName()
+  return $this->
+}
