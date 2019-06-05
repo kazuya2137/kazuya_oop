40005272a7f813b75daf0e2af67056a3f3a236bf
和也山本 <kazuya2137@gmail.com>
2019-06-05 22:15:31 +0800

    採取業務

--- index.php
+++ index.php
@@ -1,6 +1,4 @@
-<?php 
-
-//  次の「Kidクラス」を作ってください。
+<!-- //  次の「Kidクラス」を作ってください。
 
 // プロパティ：名前($name)、性別($sex)
 // メソッド：showName()
@@ -8,17 +6,50 @@
 // 名前プロパティの初期値は「Seed」、性別プロパティの初期値は「boy」にすること
 // プロパティのゲッター・セッターメソッドを用意し、メソッド経由でプロパティにアクセスできるようにすること
 // オブジェクトからプロパティ値を設定し、showNameメソッドを呼び出して出力すること
+ -->
+<?php 
 
-$name= new Name();
-$sex= new Sex();
-class Name{
+$kid = new Kid();
+$kid->setName('kazuya');
+$kid->setSex('boy');
 
-  $name;
+echo $kid->showName();
 
+class Kid{
+  private $name;
+  private $sex;
+
+  // コンストラクタ
   function __construct(){
-    $this->name = 'seed';
+    // 初期値の設定
+    $this->name = 'Seed';
     $this->sex = 'boy';
   }
-  public function showName()
-  return $this->
+
+  public function getName(){
+    return $this->name;
+  }
+
+  public function setName($namae){
+    $this->name = $namae;
+  }
+
+  public function getSex(){
+    return $this->sex;
+  }
+
+  public function setSex($seibetsu){
+     $this->sex = $seibetsu;
+   }
+
+  // 性別によって名前の語尾を判定するメソッド
+  public function showName(){
+    if ($this->sex == 'boy') {
+      return $this->name . 'くん';
+    } else if($this->sex == 'girl') {
+      return $this->name . 'ちゃん';
+    } else {
+      return $this->name .'様';
+    }
+  }
 }
